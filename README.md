6b366879412300ce31d7c07988d35f7f49b0ba50
stopthatastronaut <stopthatastronaut@gmail.com>
2017-05-17 22:51:16 +1000

    Few more things

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # boxstrapper
 Setup scripts for developer laptop and/or basic utility instances
+
+Want to run me?
+
+
+`iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/stopthatastronaut/boxstrapper/master/go.ps1'))`
--- go.ps1
+++ go.ps1
@@ -1,7 +1,6 @@
 # choco
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-
 @(
     "git",
     "poshgit",
@@ -10,16 +9,27 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/in
     "NotepadPlusPlus",
     "pscx",
     "carbon",
+    "putty",
     "awstools.powershell",
     "sysinternals",
-    "WindowsAzurePowerShell"
+    "haroopad"
 ) | % { cinst $_ -y }
 
 #psreadline
 
 Install-Package psreadline -force -skippublishercheck # why is ths not properly signed?
+Install-Module AzureRM -Force
 
 if(-not (Test-Path $home\source\repos))
 {
     New-Item $home\source\repos -force
 }
+
+New-Alias $repos $home\source\repos
+
+Start-Job { Update-Help } # in the background
+
+# Octopus deploy cmd line tools
+
+iwr https://octopus.com/downloads/latest/CommandLineTools -out-file $env:tmp\Octopus.tools.zip
+Expand-Archive $env:tmp\Octopus.tools.zip c:\Octopus\Tools -force
