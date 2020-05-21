6f3e3dc6cae3fa2e3100d1782c21df3e7330f279
Jason Brown <stopthatastronaut@gmail.com>
2020-05-21 17:57:20 +1000

    Split out VS Code icon script so we can run it standalone

--- go.ps1
+++ go.ps1
@@ -115,15 +115,4 @@ dotnet new -i Amazon.Lambda.Templates::*
 # azure functions tools
 npm i -g azure-functions-core-tools
 
-# apply the pumpkin spice icon to Visual Studio Code
-
-# it's in "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Visual Studio Code"
-
-$objShell = New-Object -comObject Shell.Application
-$objDesktop = $objShell.NameSpace("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Visual Studio Code")
-$shortcutFilename = "Visual Studio Code.lnk"
-$objFolderItem = $objDesktop.ParseName($shortcutFilename)
-$objShortcut = $objFolderItem.GetLink
-$iconpath = Resolve-Path ".\512px_visual_studio_code_1_17_icon_svg_4ba_icon.ico" | select -expand Path
-$objShortcut.SetIconLocation($iconpath,0)
-$objShortcut.Save()
+./set-vscode-icon.ps1
---
+++ set-vscode-icon.ps1
@@ -0,0 +1,14 @@
+# apply the pumpkin spice icon to Visual Studio Code
+
+# it's in "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Visual Studio Code"
+
+$objShell = New-Object -comObject Shell.Application
+$objDesktop = $objShell.NameSpace("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Visual Studio Code")
+$shortcutFilename = "Visual Studio Code.lnk"
+$objFolderItem = $objDesktop.ParseName($shortcutFilename)
+$objShortcut = $objFolderItem.GetLink
+$iconpath = Resolve-Path ".\512px_visual_studio_code_1_17_icon_svg_4ba_icon.ico" | select -expand Path
+$objShortcut.SetIconLocation($iconpath,0)
+$objShortcut.Save()
+
+Write-Host "VS Code icon set to $iconpath"
