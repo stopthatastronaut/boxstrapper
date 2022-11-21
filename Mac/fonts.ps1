c4020d00526121ecd1083771549f2f627cc1ec2b
Jason Brown <jasbro@jasbro.io>
2022-11-21 11:43:52 +1100

    starship

---
+++ Mac/fonts.ps1
@@ -0,0 +1,3 @@
+brew tap homebrew/cask-fonts &&
+brew install --cask font-fira-code-nerd-font && 
+brew install --cask font-roboto-mono-nerd-font
--- Mac/mac-specific.ps1
+++ Mac/mac-specific.ps1
@@ -5,3 +5,11 @@ Function Install-Thing {
 }
 
 brew install macdown
+
+# show all files by default
+defaults write http://com.apple.Finder AppleShowAllFiles true
+
+brew install starship
+
+'eval "$(starship init zsh)"' >> ~/.zshrc
+
