5e9ddb22055e144cf2de000f776b83504ec65e89
Jason Brown <jasbro@jasbro.io>
2024-04-22 22:57:34 +1000

    some mac tools

--- Mac/mac-specific.ps1
+++ Mac/mac-specific.ps1
@@ -7,6 +7,8 @@ Function Install-Thing {
 brew install macdown
 brew install kid3
 brew install moom
+brew install krita
+
 
 # show all files by default
 defaults write http://com.apple.Finder AppleShowAllFiles true
