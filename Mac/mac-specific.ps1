047e3fa0f93ddf790d0d2c2acb25dfc2d8c05ef6
Jason Brown <jasbro@Jasons-MBP-2.lan>
2024-04-28 23:10:31 +1000

    some extras

--- Mac/mac-specific.ps1
+++ Mac/mac-specific.ps1
@@ -9,6 +9,10 @@ brew install kid3
 brew install moom
 brew install krita
 
+brew tap azure/functions
+brew install azure-functions-core-tools@4
+# if upgrading on a machine that has 2.x or 3.x installed:
+# brew link --overwrite azure-functions-core-tools@4
 
 # show all files by default
 defaults write http://com.apple.Finder AppleShowAllFiles true
