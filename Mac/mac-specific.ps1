feddb661e56af3d848ef3bd6134e84d7f9f15944
Jason Brown <jasbro@jasbro.io>
2024-04-29 10:59:34 +1000

    tweak

--- Mac/mac-specific.ps1
+++ Mac/mac-specific.ps1
@@ -8,12 +8,22 @@ brew install macdown
 brew install kid3
 brew install moom
 brew install krita
-
+brew install finch
 
 # show all files by default
 defaults write http://com.apple.Finder AppleShowAllFiles true
 
 brew install starship
 
+if(-not (Test-Path $profile)) {
+    New-item -force $PROFILE
+}
+
+
 'eval "$(starship init zsh)"' >> ~/.zshrc
+'eval "$(starship init powershell)"' >> $profile
+
+brew install dashlane/tap/dashlane-cli
+
+
 
