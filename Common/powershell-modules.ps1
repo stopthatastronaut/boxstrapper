85f798d8fa6e39c69c591def4c02cecd564d8e81
Jason <jason@Jasons-MacBook-Pro.local>
2025-07-03 11:30:56 +1000

    update mac installation and add common

---
+++ Common/powershell-modules.ps1
@@ -0,0 +1,5 @@
+Set-PSRepository PSGallery -InstallationPolicy Trusted
+
+Install-Module AWSPowerShell
+Install-Module az
+Install-Module powershell-yaml
--- Mac/prereqs.sh
+++ Mac/prereqs.sh
@@ -1,4 +1,4 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # https://learn.microsoft.com/en-gb/powershell/scripting/install/installing-powershell-on-macos?view=powershell-7.5
 
